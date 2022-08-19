aca8c8562273d7f74f55a0013d9bf75343f13008
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2022-08-19 16:06:24 +0300

    Run dummy test

--- test_usdk_terminates.py
+++ test_usdk_terminates.py
@@ -1,2 +1,32 @@
-def test_eyes_universal_terminates():
-    pass
+import sys
+from subprocess import Popen, PIPE
+from time import sleep
+
+import psutil
+import pytest
+
+
+@pytest.mark.parametrize("reason", ["normal", "crash"])
+def test_automatic_eyes_universal_termination_on_python_termination(reason):
+    # A simple python program that executes eyes-universal and waits on stdin
+    python_process = Popen(["echo", "SDKServer"], stdin=PIPE, stdout=PIPE)
+    output_line = python_process.stdout.readline()
+    assert output_line.startswith(b"SDKServer")
+    return
+    (eyes_universal,) = psutil.Process(python_process.pid).children()
+    assert eyes_universal.is_running()
+    python_process.stdin.write(b"\n")
+    python_process.stdin.flush()
+    python_process.wait()
+    sleep(1)  # eyes-universal receives EOF on stdin and terminates, might take time
+
+    # psutils returns True when asking .is_running() on zombie process
+    # so have to check status explicitly
+    try:
+        eyes_universal_status = eyes_universal.status()
+    except psutil.NoSuchProcess:
+        eyes_universal_status = "terminated"
+    # GitHub Actions running tests in alpine container do not reap
+    # terminated eyes_universal leaving it as a zombie.
+    # Looks like there is nothing we can do about it except accept.
+    assert eyes_universal_status in ("terminated", "zombie")
