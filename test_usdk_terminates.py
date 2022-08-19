1d5248f998361e80753ea48b4147f39e03bf71d9
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2022-08-19 16:52:09 +0300

    Actual SDK test

--- test_usdk_terminates.py
+++ test_usdk_terminates.py
@@ -6,27 +6,18 @@ import psutil
 import pytest
 
 
-@pytest.mark.parametrize("reason", ["normal", "crash"])
+@pytest.mark.parametrize("reason", ["normal"])
 def test_automatic_eyes_universal_termination_on_python_termination(reason):
     # A simple python program that executes eyes-universal and waits on stdin
-    python_process = Popen(["echo", "SDKServer"], stdin=PIPE, stdout=PIPE)
+    python_process = Popen(["java", "-jar", "single-server-sdk.jar"], stdin=PIPE, stdout=PIPE)
     output_line = python_process.stdout.readline()
-    assert output_line.startswith(b"SDKServer")
-    return
+    assert output_line.startswith(b"Runner Started")
     (eyes_universal,) = psutil.Process(python_process.pid).children()
     assert eyes_universal.is_running()
-    python_process.stdin.write(b"\n")
+    termination_type = b"1\n" if reason == "normal" else b"2\n"
+    python_process.stdin.write(termination_type)
     python_process.stdin.flush()
     python_process.wait()
     sleep(1)  # eyes-universal receives EOF on stdin and terminates, might take time
 
-    # psutils returns True when asking .is_running() on zombie process
-    # so have to check status explicitly
-    try:
-        eyes_universal_status = eyes_universal.status()
-    except psutil.NoSuchProcess:
-        eyes_universal_status = "terminated"
-    # GitHub Actions running tests in alpine container do not reap
-    # terminated eyes_universal leaving it as a zombie.
-    # Looks like there is nothing we can do about it except accept.
-    assert eyes_universal_status in ("terminated", "zombie")
+    assert not eyes_universal.is_running()
