13f3ef0bd0c0bb72709d190ad1eec9705c1e7c9d
Serhii Khalymon <sergiykhalimon@gmail.com>
2022-08-25 12:11:12 +0200

    Allow to run another SDKs

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
 # Termination tests for universal bindgins
 
-## Verify that you have python installed in system
+## Local run
+### Verify that you have python installed in system
 ```
 python -V
 ```
 
-## Install deps
+### Install deps
 ```
 pyhon -m pip install -r requirements.txt
 ```
 
-## Run test on your platform
+### Run test on your platform
 ```
 pytest test_usdk_terminates.py
 ```
+
+## Add new binding
+Create binding with lang name inside `bindings` folder.
+Add your binding there and add info to `TEST_VARIANTS` const.
--- test_usdk_terminates.py
+++ test_usdk_terminates.py
@@ -1,42 +1,66 @@
 import sys
+from pathlib import Path
 from subprocess import Popen, PIPE
 from time import sleep
 
 import psutil
 import pytest
 
+platform2dir = {
+    "win32": "win",
+    "linux": "linux",
+    "darwin": "mac"
+}
+BINARIES_DIR = Path(__file__).parent / "bindings"
 
+TEST_VARIANTS = [
+    # Java
+    {"type": "java", "os": "win", "binary": BINARIES_DIR / "java" / "single-server-sdk.jar"},
+    {"type": "java", "os": "mac", "binary": BINARIES_DIR / "java" / "single-server-sdk.jar"},
+    {"type": "java", "os": "linux", "binary": BINARIES_DIR / "java" / "single-server-sdk.jar"},
+    # Other SDK's
+]
+
+PLATFORM_SPECIFIC_VARIANT = [v for v in TEST_VARIANTS if v['os'] == platform2dir[sys.platform]]
+
+
+def show_type(variant):
+    return variant['type']
+
+
+@pytest.mark.parametrize("variant", PLATFORM_SPECIFIC_VARIANT, ids=show_type)
 @pytest.mark.parametrize("reason", ["normal"])
-def test_automatic_eyes_universal_termination_on_python_termination(reason):
-    java_process = Popen(["java", "-jar", "single-server-sdk.jar"], stdin=PIPE, stdout=PIPE)
-    output_line = java_process.stdout.readline()
+def test_automatic_eyes_universal_termination_on_python_termination(reason,variant):
+    binding_process = Popen([variant['binary']], stdin=PIPE, stdout=PIPE)
+    output_line = binding_process.stdout.readline()
     assert output_line.startswith(b"Runner Started")
-    (eyes_universal,) = psutil.Process(java_process.pid).children()
+    (eyes_universal,) = psutil.Process(binding_process.pid).children()
     assert eyes_universal.is_running()
     termination_type = b"1\n" if reason == "normal" else b"2\n"
-    java_process.stdin.write(termination_type)
-    java_process.stdin.flush()
-    java_process.wait()
+    binding_process.stdin.write(termination_type)
+    binding_process.stdin.flush()
+    binding_process.wait()
     sleep(1)  # eyes-universal receives EOF on stdin and terminates, might take time
 
     assert not eyes_universal.is_running()
 
 
+@pytest.mark.parametrize("variant", PLATFORM_SPECIFIC_VARIANT,ids=show_type)
 @pytest.mark.parametrize("reason", ["normal"])
-def test_parallel_eyes_universal_termination_on_python_termination(reason):
+def test_parallel_eyes_universal_termination_on_python_termination(reason,variant):
     processes = []
     for _ in range(2):
-        java_process = Popen(["java", "-jar", "single-server-sdk.jar"], stdin=PIPE, stdout=PIPE)
-        output_line = java_process.stdout.readline()
+        binding_process = Popen([variant['binary']], stdin=PIPE, stdout=PIPE)
+        output_line = binding_process.stdout.readline()
         assert output_line.startswith(b"Runner Started")
-        (eyes_universal,) = psutil.Process(java_process.pid).children()
+        (eyes_universal,) = psutil.Process(binding_process.pid).children()
         assert eyes_universal.is_running()
-        processes.append((java_process, eyes_universal))
+        processes.append((binding_process, eyes_universal))
 
-    for java_process, eyes_universal in processes:
+    for binding_process, eyes_universal in processes:
         termination_type = b"1\n" if reason == "normal" else b"2\n"
-        java_process.stdin.write(termination_type)
-        java_process.stdin.flush()
-        java_process.wait()
+        binding_process.stdin.write(termination_type)
+        binding_process.stdin.flush()
+        binding_process.wait()
         sleep(1)  # eyes-universal receives EOF on stdin and terminates, might take time
         assert not eyes_universal.is_running()
